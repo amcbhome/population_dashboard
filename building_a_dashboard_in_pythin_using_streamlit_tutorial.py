7fb7115ca30891a6a2ee151705b7d768eb0464ec
amcbhome <110579154+amcbhome@users.noreply.github.com>
2025-04-26 05:39:33 +0100

    Update building_a_dashboard_in_pythin_using_streamlit_tutorial.py

--- building_a_dashboard_in_pythin_using_streamlit_tutorial.py
+++ building_a_dashboard_in_pythin_using_streamlit_tutorial.py
@@ -31,7 +31,8 @@ st.set_page_config(
     page_title="US Population Dashboard",
     page_icon="🏂",
     layout="wide",
-    initial_sidebar_state="expanded")
+    initial_sidebar_state="expanded"
+)
 
 alt.themes.enable("dark")
 
@@ -56,30 +57,29 @@ with st.sidebar:
 """# 3.5 Plot and chart types"""
 
 # Heatmap
-
 def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
     heatmap = alt.Chart(input_df).mark_rect().encode(
-            y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="Year", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
-            x=alt.X(f'{input_x}:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
-            color=alt.Color(f'max({input_color}):Q',
-                             legend=None,
-                             scale=alt.Scale(scheme=input_color_theme)),
-            stroke=alt.value('black'),
-            strokeWidth=alt.value(0.25),
-        ).properties(width=900
-        ).configure_axis(
+        y=alt.Y(f'{input_y}:O', axis=alt.Axis(title="Year", titleFontSize=18, titlePadding=15, titleFontWeight=900, labelAngle=0)),
+        x=alt.X(f'{input_x}:O', axis=alt.Axis(title="", titleFontSize=18, titlePadding=15, titleFontWeight=900)),
+        color=alt.Color(f'max({input_color}):Q',
+                         legend=None,
+                         scale=alt.Scale(scheme=input_color_theme)),
+        stroke=alt.value('black'),
+        strokeWidth=alt.value(0.25),
+    ).properties(width=900
+    ).configure_axis(
         labelFontSize=12,
         titleFontSize=12
-        )
+    )
     # height=300
     return heatmap
 
 # Choropleth
 def make_choropleth(input_df, input_id, input_column, input_color_theme):
     choropleth = px.choropleth(
-        input_df, 
-        locations=input_id, 
-        color=input_column, 
+        input_df,
+        locations=input_id,
+        color=input_column,
         locationmode="USA-states",
         color_continuous_scale=input_color_theme,
         range_color=(0, max(input_df.population)),
@@ -95,58 +95,53 @@ def make_choropleth(input_df, input_id, input_column, input_color_theme):
     )
     return choropleth
 
-    # Donut
-
+# Donut
 def calculate_population_difference(input_df, input_year):
-  selected_year_data = input_df[input_df['year'] == input_year].reset_index()
-  previous_year_data = input_df[input_df['year'] == input_year - 1].reset_index()
-  selected_year_data['population_difference'] = selected_year_data.population.sub(previous_year_data.population, fill_value=0)
-  return pd.concat([selected_year_data.states, selected_year_data.id, selected_year_data.population, selected_year_data.population_difference], axis=1).sort_values(by="population_difference", ascending=False)
+    selected_year_data = input_df[input_df['year'] == input_year].reset_index()
+    previous_year_data = input_df[input_df['year'] == input_year - 1].reset_index()
+    selected_year_data['population_difference'] = selected_year_data.population.sub(previous_year_data.population, fill_value=0)
+    return pd.concat([selected_year_data.states, selected_year_data.id, selected_year_data.population, selected_year_data.population_difference], axis=1).sort_values(by="population_difference")
 
 def make_donut(input_response, input_text, input_color):
-  if input_color == 'blue':
-      chart_color = ['#29b5e8', '#155F7A']
-  if input_color == 'green':
-      chart_color = ['#27AE60', '#12783D']
-  if input_color == 'orange':
-      chart_color = ['#F39C12', '#875A12']
-  if input_color == 'red':
-      chart_color = ['#E74C3C', '#781F16']
+    if input_color == 'blue':
+        chart_color = ['#29b5e8', '#155F7A']
+    if input_color == 'green':
+        chart_color = ['#27AE60', '#12783D']
+    if input_color == 'orange':
+        chart_color = ['#F39C12', '#875A12']
+    if input_color == 'red':
+        chart_color = ['#E74C3C', '#781F16']
 
-  source = pd.DataFrame({
-      "Topic": ['', input_text],
-      "% value": [100-input_response, input_response]
-  })
-  source_bg = pd.DataFrame({
-      "Topic": ['', input_text],
-      "% value": [100, 0]
-  })
+    source = pd.DataFrame({
+        "Topic": ['', input_text],
+        "% value": [100-input_response, input_response]
+    })
+    source_bg = pd.DataFrame({
+        "Topic": ['', input_text],
+        "% value": [100, 0]
+    })
 
-  plot = alt.Chart(source).mark_arc(innerRadius=45, cornerRadius=25).encode(
-      theta="% value",
-      color= alt.Color("Topic:N",
-                      scale=alt.Scale(
-                          #domain=['A', 'B'],
-                          domain=[input_text, ''],
-                          # range=['#29b5e8', '#155F7A']),  # 31333F
-                          range=chart_color),
-                      legend=None),
-  ).properties(width=130, height=130)
+    plot = alt.Chart(source).mark_arc(innerRadius=45, cornerRadius=25).encode(
+        theta="% value",
+        color=alt.Color("Topic:N",
+                        scale=alt.Scale(
+                            domain=[input_text, ''],
+                            range=chart_color),
+                        legend=None),
+    ).properties(width=130, height=130)
 
-  text = plot.mark_text(align='center', color="#29b5e8", font="Lato", fontSize=32, fontWeight=700, fontStyle="italic").encode(text=alt.value(f'{input_response} %'))
-  plot_bg = alt.Chart(source_bg).mark_arc(innerRadius=45, cornerRadius=20).encode(
-      theta="% value",
-      color= alt.Color("Topic:N",
-                      scale=alt.Scale(
-                          # domain=['A', 'B'],
-                          domain=[input_text, ''],
-                          range=chart_color),  # 31333F
-                      legend=None),
-  ).properties(width=130, height=130)
-  return plot_bg + plot + text
+    text = plot.mark_text(align='center', color="#29b5e8", font="Lato", fontSize=32, fontWeight=700, fontStyle="italic").encode(text=alt.value(f'{input_response} %'))
+    plot_bg = alt.Chart(source_bg).mark_arc(innerRadius=45, cornerRadius=20).encode(
+        theta="% value",
+        color=alt.Color("Topic:N",
+                        scale=alt.Scale(
+                            domain=[input_text, ''],
+                            range=chart_color),
+                        legend=None),
+    ).properties(width=130, height=130)
+    return plot_bg + plot + text
 
 # Convert population to text
-
 def format_number(num):
     if num > 1000000:
         if not num % 1000000:
@@ -185,16 +180,12 @@ with col[0]:
         last_state_delta = ''
     st.metric(label=last_state_name, value=last_state_population, delta=last_state_delta)
 
-
     st.markdown('#### States Migration')
 
     if selected_year > 2010:
-        # Filter states with population difference > 50000
-        # df_greater_50000 = df_population_difference_sorted[df_population_difference_sorted.population_difference_absolute > 50000]
         df_greater_50000 = df_population_difference_sorted[df_population_difference_sorted.population_difference > 50000]
         df_less_50000 = df_population_difference_sorted[df_population_difference_sorted.population_difference < -50000]
 
-        # % of States with population difference > 50000
         states_migration_greater = round((len(df_greater_50000)/df_population_difference_sorted.states.nunique())*100)
         states_migration_less = round((len(df_less_50000)/df_population_difference_sorted.states.nunique())*100)
         donut_chart_greater = make_donut(states_migration_greater, 'Inbound Migration', 'green')
@@ -212,7 +203,6 @@ with col[0]:
         st.write('Outbound')
         st.altair_chart(donut_chart_less)
 
-
 # Column 2 Maps
 with col[1]:
     st.markdown('#### Total Population')
@@ -223,8 +213,7 @@ with col[1]:
     heatmap = make_heatmap(df_reshaped, 'year', 'states', 'population', selected_color_theme)
     st.altair_chart(heatmap, use_container_width=True)
 
-    # Column 3 Results/About
-
+# Column 3 Results/About
 with col[2]:
     st.markdown('#### Top States')
 
@@ -233,23 +222,22 @@ with col[2]:
                  hide_index=True,
                  width=None,
                  column_config={
-                    "states": st.column_config.TextColumn(
-                        "States",
-                    ),
-                    "population": st.column_config.ProgressColumn(
-                        "Population",
-                        format="%f",
-                        min_value=0,
-                        max_value=max(df_selected_year_sorted.population),
+                     "states": st.column_config.TextColumn(
+                         "States",
+                     ),
+                     "population": st.column_config.ProgressColumn(
+                         "Population",
+                         format="%f",
+                         min_value=0,
+                         max_value=max(df_selected_year_sorted.population),
                      )}
                  )
 
-with st.expander('About', expanded=True):
+    with st.expander('About', expanded=True):
         st.write('''
             - Data: [U.S. Census Bureau](<https://www.census.gov/data/datasets/time-series/demo/popest/2010s-state-total.html>).
             - :orange[**Gains/Losses**]: states with high inbound/ outbound migration for selected year
             - :orange[**States Migration**]: percentage of states with annual inbound/ outbound migration > 50,000
-            ''')
+        ''')
 
 """# 3.7 Deploying the dashboard app to the cloud"""
-
